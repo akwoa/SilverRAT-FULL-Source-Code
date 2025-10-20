912f34e98150800663bdd58345e82352897e7a0c
Denis T. <deadspell21391@gmail.com>
2025-10-20 13:03:04 +0200

    AssemblyInfo.cs

---
+++ SRC/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SilverRAT")]
+[assembly: AssemblyConfiguration("cracked")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("SilverRAT")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("C3AEE5F3-98FC-4415-94AD-40741CDA4ACC")]
+[assembly: AssemblyFileVersion("6.48.171.1501")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyVersion("6.48.171.1501")]
